05d01bd303d7e4a20a35c91e71fe79403e756147
JD <jdakemp@gmail.com>
2022-10-10 21:10:50 -0400

    Completely stored student object in Student Array with all data properties

--- src/main/java/com/coderscampus/assignment_4/Requirements.java
+++ src/main/java/com/coderscampus/assignment_4/Requirements.java
@@ -8,66 +8,44 @@ import com.coderscampus.myapp.service.StudentService;
 
 public class Requirements {
 
-//	public String filterStudents() throws IOException {
-//		BufferedReader studentInfo = null; 
-//		
-//		try {
-//			studentInfo = new BufferedReader(new FileReader("student-master-list.csv"));
-//			System.out.println(studentInfo.readLine());
-//		} finally {
-//			if (studentInfo != null ) studentInfo.close();
-//		}
-//	}
-
 	public static void main(String[] args) throws IOException {
-		// I need to separate data into 3 different CVS files, each file should contain
-		// a list of students
-		// specific to that particular course (grouping the students by course)
 
-		// for each of the 3 CVS files, need to sort the students by grade in descending
-		// order
-
-		// need to import the data from CVS, sort the data, then create new files with
-		// the appropriate information
-		// in the file
-
-		BufferedReader studentInfo = null; 
-		Boolean matchFound;
-		Integer[] numMatch = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		BufferedReader studentInfo = null;
+		Integer stuIdandGrade;
 		Student[] allStudents = new Student[100];
-
+		int i = 0;
 		try {
 			studentInfo = new BufferedReader(new FileReader("student-master-list.csv"));
-			while (studentInfo.readLine() != null) {
+			studentInfo.readLine();
+			while (i < 100) {
 				String info = studentInfo.readLine();
-				String[] allStudentInfo = info.split(",");   
+				String[] allStudentInfo = info.split(",");
+
+//				for (String data : allStudentInfo) {
+//					Boolean numMatch = data.matches("^[1-9][0-9]?$|^100$");
+//
+//					if (numMatch) {
+//						Integer intData = Integer.valueOf(data);
+//						stuIdandGrade = intData;
+//					}
+//				}
 				
-				for (String data : allStudentInfo ) {
-					for (int i = 0; i < numMatch.length; i++) {
-					if ( Integer.valueOf(data)== numMatch[i]) {
-						Integer intData = Integer.valueOf(data);
-						
-				Student students = StudentService.createStudent(allStudentInfo[intData], allStudentInfo[1],
-								allStudentInfo[2], allStudentInfo[intData]); 
-				allStudents[i] = students;
-					}
-					
-					//Student students = StudentService.createStudent()
-					}
-				}
-				
-				Student students = StudentService.createStudent(allStudentInfo[0], allStudentInfo[1],
-						allStudentInfo[2], allStudentInfo[3]);
-				for (String st : finalStudentInfo) {
-				System.out.println(st);
-				}
+				Student student = StudentService.createStudent(Integer.valueOf(
+						allStudentInfo[0]), 
+						allStudentInfo[1],
+						allStudentInfo[2],
+						Integer.valueOf(allStudentInfo[3]));
+
+				allStudents[i++] = student; 
+				System.out.println(i);
+
 			}
+
 		} finally {
 			if (studentInfo != null)
 				studentInfo.close();
 		}
-		
-		
+
 	}
 
 }
