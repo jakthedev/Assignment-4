13fb569171db2ad9db37df0ec0f12acb576b2122
JD <jdakemp@gmail.com>
2022-10-12 08:39:21 -0400

    Removed all of the Null vaules from the array

--- src/main/java/com/coderscampus/assignment_4/Requirements.java
+++ src/main/java/com/coderscampus/assignment_4/Requirements.java
@@ -3,6 +3,8 @@ package com.coderscampus.assignment_4;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 
 import com.coderscampus.myapp.service.StudentService;
 
@@ -11,14 +13,14 @@ public class Requirements {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader studentInfo = null;
-		Integer stuIdandGrade;
 
 		Student[] allStudents = new Student[100];
 		Student[] compSci = new Student[101];
 		Student[] stat = new Student[101];
 		Student[] apmth = new Student[101];
 
-		int i = 0;
+		int i = 0; 
+		int j = 0;
 		
 		try {
 			studentInfo = new BufferedReader(new FileReader("student-master-list.csv"));
@@ -38,31 +40,29 @@ public class Requirements {
 
 				for (Student stu : allStudents) {
 
-					for (int j = 0; j < allStudents.length; j++) {
-
 						if (stu.getCoursename().startsWith("COMP")) {
-							compSci[j++] = stu;
-							stu = allStudents[j++];
-
+							compSci[j] = stu;
+							stu = allStudents[j];
+							j++;
 						}
 						if (stu.getCoursename().startsWith("STAT")) {
-							stat[j++] = stu;
-							stu = allStudents[j++];
-
+							stat[j] = stu;
+							stu = allStudents[j];
+							j++;
 						}
 						if (stu.getCoursename().startsWith("APMTH")) {
-							apmth[j++] = stu;
-							stu = allStudents[j++];
-
-						} 
+							apmth[j] = stu;
+							stu = allStudents[j]; 
+							j++;
+						}
 					}
-				}
-			
-
 		} finally {
 			if (studentInfo != null)
 				studentInfo.close();
 		}
+		
+		Student[] cleanedCompSci = Arrays.stream(compSci).filter(Objects::nonNull).toArray(Student[]::new); 
+		System.out.println(Arrays.toString(cleanedCompSci));
 	}
 
 }
